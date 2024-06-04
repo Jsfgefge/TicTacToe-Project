6e74d6b7fe95ec6f8b368ca075e6d51dff1936d1
Jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2024-06-04 17:13:37 -0600

    Changes in the logic

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
 function Gameboard() {
-  let gameState = [['_','_','_'],
-                   ['_','_','_'],
-                   ['_','_','_']];
+  let gameState = [
+    ["_", "_", "_"],
+    ["_", "_", "_"],
+    ["_", "_", "_"],
+  ];
   let myState = "";
-  const newMove = (x,y,z) => gameState[x][y]= (z==0)?"x":"o";
-  const getCurrentSate = function() {for (let i= 0; i< 3; i++) {
-    for (let j= 0; j < 3; j++) {
-      myState+=gameState[i][j]+" ";
+  let isGameEnded = false;
+
+  const getCurrentSate = function () {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        myState += gameState[i][j] + " ";
+      }
+      myState += "\n";
     }
-    myState+="\n";
-  }
-    console.log(myState);
+    return myState;
   };
-  return {newMove, gameState, getCurrentSate, myState};
+
+  return { gameState, getCurrentSate, myState, isGameEnded };
 }
 
-function NewPlayer(){
+function GameFlow(gameArr) {
+  const newMove = (x, y, z) => (gameArr[x][y] = z == 0 ? "x" : "o");
 
+  const checkWin = function (isGameEnded) {
+    if(!isGameEnded){
+      
+    }
+  };
+
+  return { newMove, gameArr, checkWin };
 }
 
+function NewPlayer() {}
+
+//testing-------------------------
 const newGame = Gameboard();
-newGame.newMove(1,1,1);
-newGame.newMove(0,1,0);
-newGame.newMove(1,2,1);
-newGame.newMove(0,2,0);
+const gameFlow = GameFlow(newGame.gameState);
 
-newGame.getCurrentSate();
-console.table({
-  myBoardState: newGame.gameState
-})
+gameFlow.newMove(1, 1, 1);
+gameFlow.newMove(1, 2, 0);
+gameFlow.newMove(2, 1, 0);
+gameFlow.newMove(1, 0, 1);
+gameFlow.newMove(0, 1, 1);
 
+// newGame.newMove(1,1,1);
+// newGame.newMove(0,1,0);
+// newGame.newMove(1,2,1);
+// newGame.newMove(0,2,0);
 
-console.log(newGame.myState);
+// newGame.getCurrentSate();
+
+console.log(newGame.getCurrentSate());
+// console.table({
+//   myBoardState: newGame.gameState
+// })
+
+console.log(gameFlow.gameArr, "hola");
